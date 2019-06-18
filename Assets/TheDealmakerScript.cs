b2f1d1b63e9e4475fa95f0f761acbd2a6b603bda
GHXX <GHXX@gmx.at>
2019-06-18 04:33:01 +0200

    renamed module, Fixed curency order

--- Assets/TheDealmakerScript.cs
+++ Assets/TheDealmakerScript.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class DealOrNoDealScript : MonoBehaviour
+public class TheDealmakerScript : MonoBehaviour
 {
 
     public KMAudio Audio;
